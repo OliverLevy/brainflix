8b1b3bec76eda73ace4253c0269686ed524e27d1
Oliver Levy <oliver.levy.ol@gmail.com>
2020-06-15 19:02:11 -0400

    adding assets

---
+++ sprint-1/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+   <header>
+     <img src="../assets/Logo/Logo-brainflix.png" alt=""/>
+   </header>
+  )
+}
+
+export default Header;
