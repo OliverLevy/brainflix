075804b0cf269a888d466f2507cc9bb58d062e16
Oliver Levy <oliver.levy.ol@gmail.com>
2020-06-15 19:44:06 -0400

    react header is working! Needs styling

--- sprint-1/src/components/Header.js
+++ sprint-1/src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import logo from '../assets/Logo/Logo-brainflix.png'
+import avatar from '../assets/Images/Mohan-muruge.jpg'
+import '../styles/Header.scss'
+import searchIcon from '../assets/Icons/PNG/Icon-search.png'
+
 
 function Header() {
   return (
-   <header>
-     <img src="../assets/Logo/Logo-brainflix.png" alt=""/>
-   </header>
+    <header className="header">
+      <img src={logo} alt="Brainflix logo" className="header__logo" />
+      <div className="header__item container">
+        <input type="search" className="header__search" placeholder="Search" />
+        <div>
+          <button className="header__upload btn">+ UPLOAD</button>
+          <div className="header__avatar-container">
+            <img src={avatar} alt="avatar" className="header__avatar" />
+          </div>
+        </div>
+      </div>
+    </header>
+
   )
 }
 
