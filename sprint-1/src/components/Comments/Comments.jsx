fdc09bf3546934ceff1b80f931e59b6a4f9fd89b
Oliver Levy <oliver.levy.ol@gmail.com>
2020-06-21 13:16:35 -0400

    comments css is good but needs work

--- sprint-1/src/components/Comments/Comments.jsx
+++ sprint-1/src/components/Comments/Comments.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Comments.scss'
+import Avatar from '../../assets/Images/Mohan-muruge.jpg'
 
 
 function Comments({submitHandler, comments}){
@@ -10,10 +11,23 @@ function Comments({submitHandler, comments}){
     <div>
       <div className="add-comment">
         <h3>3 Comments</h3>
-        <form onSubmit={submitHandler} className="add-comment__form">
-          <textarea name="commentBox"></textarea>
-          <button>COMMENT</button>
-        </form>
+
+        <div className="add-comment__card">
+          <div className="add-comment__avatar-container">
+              <img
+              className="add-comment__avatar" 
+              src={Avatar} 
+              alt="user-avatar"
+              />
+            </div>
+
+          <form onSubmit={submitHandler} className="add-comment__form">
+            <h5>Join the conversation</h5>
+            <textarea className="add-comment__input" name="commentBox"></textarea>
+            <button className="add-comment__btn btn">COMMENT</button>
+          </form>
+        </div>
+
       </div>
       <div>
         {comments.map((comment, id) => {
