1c31fd54ae6d6e2127330674d04994b902f44125
Oliver Levy <oliver.levy.ol@gmail.com>
2020-06-21 11:33:42 -0400

    fixed header css and add descriptions css

--- sprint-1/src/components/Description/Description.jsx
+++ sprint-1/src/components/Description/Description.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import ViewsIcon from '../../assets/Icons/SVG/Icon-views.svg'
+import LikesIcon from '../../assets/Icons/SVG/Icon-likes.svg'
 
 import './Description.scss'
 
 
 function Description({mainData}){
   return(
-    <div>
-          <h1>{mainData.title}</h1>
-          <h3>By {mainData.channel}</h3>
-          <p>{mainData.timestamp}</p>
-          <p>{mainData.views}</p>
-          <p>{mainData.likes}</p>
-          <p>{mainData.description}</p>
+    <div className="desc">
+          <h1 className="desc__title">{mainData.title}</h1>
+          
+          <div className="desc__items">
+            <div className="desc__channel-container">
+              <h3 className="desc__channel">By {mainData.channel}</h3>
+              <p>{mainData.timestamp}</p>
+            </div>
+
+            <div className="desc__stats">
+              <img 
+              className="desc__views-icon" 
+              src={ViewsIcon} 
+              alt="views-icon"/>
+              <p>{mainData.views}</p>
+              <img 
+              className="desc__likes-icon" 
+              src={LikesIcon} 
+              alt="likes-icon"/>
+              <p>{mainData.likes}</p>
+            </div>
+          </div>
+
+          <div className="divider"></div>
+
+          <p className="desc__text">{mainData.description}</p>
     </div>
   )
 }
